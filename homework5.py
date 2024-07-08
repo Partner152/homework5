0ea0c2e6880b8eb8c28c1749a67c71d015ca7f22
Partner152 <partner1780@mail.ru>
2024-07-08 09:47:36 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var=(1,2,3,'a','b')
+print(immutable_var)
+#immutable_var[0]=7 элементы кортежа неизменны,в отличии от списков
+mutable_list=[1,4,7,'list','objekt']
+mutable_list[1]=5,9
+mutable_list[2]='встреча'
+print(mutable_list)
